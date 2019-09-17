8e422ee35935647e91777a1ab325080870e2e0a2
hiyongrui <172808H@mymail.nyp.edu.sg>
2019-09-17 17:26:48 +0800

    ionic accordion expandable
    https://www.youtube.com/watch?v=0-goeHxBc8c

---
+++ src/app/components/expandable/expandable.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-expandable',
+  templateUrl: './expandable.component.html',
+  styleUrls: ['./expandable.component.scss'],
+})
+export class ExpandableComponent implements OnInit {
+
+  ngOnInit() {
+    console.warn("hello")
+  }
+
+  @Input('expanded') expanded;
+  @Input('expandHeight') expandedHeight;
+  
+  currentHeight: number = 0;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    console.warn(this.expanded);
+    console.warn(this.expandedHeight);
+  }
+
+
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  items: any = new Array(10);
+  // itemExpanded: boolean = true;
+  itemExpandedHeight: number = 200;
+  //https://www.youtube.com/watch?v=0-goeHxBc8c
+  constructor() {
+    this.items = [
+      {expanded: false},
+      {expanded: true},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+    ]
+  }
+
+  expandItem(item) {
+    item.expanded = !item.expanded
+  }
+
+}
